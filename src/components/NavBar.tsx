f013963a4daa983b710e61257624cc689e6a1531
Prateek Das <prateek17das@gmail.com>
2024-06-03 23:35:40 +0600

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+import logo from '../assets/flower.png'
+import { px } from 'framer-motion'
+
+function NavBar() {
+  return (
+    <HStack>
+        <Image src = {logo} boxSize='60px'/>
+        <Text>Navbar</Text>
+    </HStack>
+  )
+}
+ 
+export default NavBar
