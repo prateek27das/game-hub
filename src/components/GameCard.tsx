e650f5ab0dd072eebcf016aa6eaadc54b76b7226
Prateek Das <prateek17das@gmail.com>
2024-06-06 23:54:11 +0600

    Implemented Console Name

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
 
 interface Props {
     game: Game
@@ -14,6 +14,8 @@ function GameCard({game} : Props )  {
             <Heading fontSize='2xl'>
                  {game.name}
             </Heading>
+            {game.parent_platforms.map(({platform}) => <Text>{platform.name}</Text>)}
+             
         </CardBody>
     </Card>
    )
