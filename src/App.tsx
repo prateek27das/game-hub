9f8b7adb025e01e6ebf679c830ac62b672e95bb1
Prateek Das <prateek17das@gmail.com>
2024-06-11 23:55:40 +0600

    Fetch GenreList

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
-import NavBar from './components/NavBar'
-import GameGrid from './components/GameGrid'
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
-  return <Grid templateAreas={{
-    base : '"nav" "main"',
-    lg : '"nav nav" "aside main"'
-  }}>
-    <GridItem area = "nav">
-      <NavBar/>
-    </GridItem>
-    <Show above='lg'>
-    <GridItem area = "aside" >
-      aside
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" "main"',
+        lg: '"nav nav" "aside main"',
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
-    </Show>
+      <Show above="lg">
+        <GridItem area="aside">
+          <GenreList />
+        </GridItem>
+      </Show>
 
-    <GridItem area = "main" >
-      <GameGrid/>
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
-  </Grid>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
