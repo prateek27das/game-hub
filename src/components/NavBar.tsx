08700bda02ee8b23d5b7bc65e7aee26e73ff1242
Prateek Das <prateek17das@gmail.com>
2024-06-21 20:04:49 +0600

    Refactor: Extract a query object

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,16 @@
-import { HStack, Image } from '@chakra-ui/react'
-import React from 'react'
-import logo from '../assets/flower.png'
-import { px } from 'framer-motion'
-import ColorModeSwitch from './ColorModeSwitch'
+import { HStack, Image } from "@chakra-ui/react";
+import React from "react";
+import logo from "../assets/flower.png";
+import { px } from "framer-motion";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
   return (
-    <HStack justifyContent= 'space-between' padding='10px'>
-        <Image src = {logo} boxSize='60px'/>
-        <ColorModeSwitch/>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
     </HStack>
-  )
+  );
 }
- 
-export default NavBar 
+
+export default NavBar;
