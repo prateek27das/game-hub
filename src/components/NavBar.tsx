7ddb2fd189ea9ad3d1e0d51fad4152ba14e82aad
Prateek Das <prateek17das@gmail.com>
2024-06-24 23:33:43 +0600

    Change Logo in Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { HStack, Image } from "@chakra-ui/react";
 import React from "react";
-import logo from "../assets/flower.png";
+// import logo from "../assets/flower.png";
+import logo from "../assets/logo.webp";
 import { px } from "framer-motion";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
