a1b156c2cfb701834aa046135220a177f1c6747b
Prateek Das <prateek17das@gmail.com>
2024-07-29 19:51:31 +0600

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,10 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
-import { Platform } from "./hooks/useGames";
+
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
+import { Platform } from "./hooks/usePlatform";
 
 export interface GameQuery {
   genre: Genre | null;
--- src/components/PlatformiconList.tsx
+++ src/components/PlatformiconList.tsx
@@ -10,9 +10,10 @@ import {
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
+
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
+import { Platform } from "../hooks/usePlatform";
 
 interface Props {
   platforms: Platform[];
