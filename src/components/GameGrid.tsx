0586feb5df71954f9efbe0a2bb01b4aa40fb2dfc
Prateek Das <prateek17das@gmail.com>
2024-07-29 19:49:10 +0600

    Refactoring

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
